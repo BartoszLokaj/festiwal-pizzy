39d93c172e4dc011d25d0a7c3294f6b8b802f173
Bartosz <bartoszlokaj@gmail.com>
2018-11-20 13:33:04 +0100

    ifs in js

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ function countdown(){
     var currentTime = new Date().getTime();
     var difference = festivalTime - currentTime;
     var days =  Math.floor(difference / (1000 * 60*60*24));
+    if (days<10)days = "0" + days;
     var hours = Math.floor(difference / (1000 * 60*60) % 24);
+    if(hours<10)hours = "0" + hours;
     var minutes = Math.floor(difference / (1000 * 60) % 60);
+    if(minutes<10)minutes = "0" + minutes;
     var seconds = Math.floor(difference / (1000) % 60);
+    if(seconds<10)seconds = "0" + seconds;
     // document.querySelector(".clock").innerHTML = days + "dni" + hours + "godzin" + minutes + "minut" + seconds + "sekund";
     document.querySelector(".clock").innerHTML = `${days} dni ${hours} godzin ${minutes} minut ${seconds} sekund`;
 }
